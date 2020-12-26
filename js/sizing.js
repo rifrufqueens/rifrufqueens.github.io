4b519ee2d566feb3052f164b0645e67e4ccd30a9
Vincent <vzhang517@gmail.com>
2020-12-26 15:30:35 -0600

    adding pixel 2xl

--- js/sizing.js
+++ js/sizing.js
@@ -5,3 +5,7 @@ $("#devices").change(function () {
   $("#sizing-image").removeClass();
   $("#sizing-image").addClass(firstDropVal);
 });
+
+document.addEventListener('touchmove', function (event) {
+  if (event.scale !== 1) { event.preventDefault(); }
+}, false);
